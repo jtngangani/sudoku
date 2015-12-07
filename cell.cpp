75b38eefa8a237710cb49a586ee306723be99c04
Jatin Gangani <jtngangani@gmail.com>
2015-12-07 01:30:31 -0800

    Initial commit of cell class

---
+++ cell.cpp
@@ -0,0 +1,36 @@
+#include <cell.h>
+
+using namespace std;
+
+cell::cell()
+{
+    m_valid = false;
+    m_value = INT_MAX;
+}
+
+int cell::getValue()
+{
+    assert(isValid());
+    return m_value;
+}
+
+bool cell::isValid()
+{
+    return m_valid;
+}
+
+void cell::setValid(bool p_valid)
+{
+    m_valid = p_valid;
+    if(not m_valid)
+        m_value = INT_MAX;
+}
+
+voidcell:: setValue(int p_value)
+{
+    m_valid = true;
+    m_value = p_value;
+}
+
+
+        
